270d6f0555a865d1c50cc05fb8cfd14ea6cd5097
Sokratis Ousakof <skathelast@hotmail.com>
2020-07-13 21:44:47 +0100

    Added basic MVC

--- src/MinesweeperModel.java
+++ src/MinesweeperModel.java
@@ -1,2 +1,30 @@
 public class MinesweeperModel {
+
+    private int minefieldX, minefieldY;
+    private int[][] minefield;
+
+    public void initialiseMinefield()
+    {
+        this.minefield = new int[this.minefieldX][this.minefieldY];
+    }
+
+    public void setXY(int minefieldX,int minefieldY)
+    {
+        this.minefieldX = minefieldX;
+        this.minefieldY = minefieldY;
+
+    }
+
+    public int getMinefieldX() {
+        return minefieldX;
+    }
+
+    public int getMinefieldY() {
+        return minefieldY;
+    }
+
+    public int[][] getMinefield() {
+        return minefield;
+    }
+
 }
