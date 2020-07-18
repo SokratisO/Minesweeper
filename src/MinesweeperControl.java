052ab8278e053f1d4c0d98958b94ff586f194e05
Sokratis Ousakof <skathelast@hotmail.com>
2020-07-18 02:54:53 +0100

    difficulty selection works properly

--- src/MinesweeperControl.java
+++ src/MinesweeperControl.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionListener;
 public class MinesweeperControl {
     MinesweeperView theView;
     MinesweeperModel theModel;
+    private int numberOfMines;
     MinesweeperControl(MinesweeperView theView,MinesweeperModel theModel){
         this.theView = theView;
         this.theModel = theModel;
@@ -17,12 +18,52 @@ public class MinesweeperControl {
         public void actionPerformed(ActionEvent e) {
             if(e.getSource() == theView.easyButton)
             {
-                theView.selectDifficultyFrame.setVisible(false);
-                theView.gameFrame.setVisible(true);
-                theModel.initialiseMinefield();
-                theModel.placeMines(25);
+                initialiseGameBoard(easyPreset());
+                theView.consoleTextAreaTBR.setText(theModel.displayMinefieldArray());
+            }
+            if(e.getSource() == theView.normalButton)
+            {
+                initialiseGameBoard(normalPreset());
+                theView.consoleTextAreaTBR.setText(theModel.displayMinefieldArray());
+            }
+            if(e.getSource() == theView.hardButton)
+            {
+                initialiseGameBoard(hardPreset());
                 theView.consoleTextAreaTBR.setText(theModel.displayMinefieldArray());
             }
         }
+    }
+
+    private int easyPreset(){
+        numberOfMines = 25;
+        theModel.setXY(10,10);
+
+        return numberOfMines;
+    }
+
+    private int normalPreset(){
+        numberOfMines = 100;
+        theModel.setXY(20,20);
+        theView.gameFrame.setSize(300,300);
+        return numberOfMines;
+    }
+
+    private int hardPreset(){
+        numberOfMines = 250;
+        theModel.setXY(30,30);
+        theView.gameFrame.setSize(500,500);
+        return numberOfMines;
+    }
+
+    private void switchFrames(){
+        theView.selectDifficultyFrame.setVisible(false);
+        theView.gameFrame.setVisible(true);
+    }
+
+    private void initialiseGameBoard(int numberOfMines)
+    {
+        theModel.initialiseMinefield();
+        theModel.placeMines(numberOfMines);
+        switchFrames();
     }
 }
--- src/MinesweeperView.java
+++ src/MinesweeperView.java
@@ -4,36 +4,54 @@ import java.awt.event.ActionListener;
 
 public class MinesweeperView {
 
+    private final GridBagConstraints layoutConstraints = new GridBagConstraints();
     JFrame selectDifficultyFrame = new JFrame(), gameFrame = new JFrame();
     JPanel difficultyPanel = new JPanel(), gamePanel = new JPanel();
-    final Dimension SELECT_FRAME_DIMENSION = new Dimension(500,500);
+    final Dimension SELECT_FRAME_DIMENSION = new Dimension(200,300),
+            GAME_FRAME_DIMENSION = new Dimension(300,300);
     JButton easyButton = new JButton("Easy"), normalButton = new JButton("Normal"),
             hardButton = new JButton("Hard");
     JTextArea consoleTextAreaTBR = new JTextArea();
 
     MinesweeperView(){
+        //initialise selection frame
         selectDifficultyFrame.setTitle("Minesweeper");
         selectDifficultyFrame.setSize(SELECT_FRAME_DIMENSION);
         selectDifficultyFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         selectDifficultyFrame.setLocationRelativeTo(null);
         selectDifficultyFrame.setResizable(false);
 
+        //initialise game frame
         gameFrame.setTitle("Minesweeper");
-        gameFrame.setSize(SELECT_FRAME_DIMENSION);
+        gameFrame.setSize(GAME_FRAME_DIMENSION);
         gameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         gameFrame.setLocationRelativeTo(null);
         gameFrame.setResizable(false);
 
+        //other GUI stuff to be added
         selectDifficultyFrame.add(difficultyPanel);
-
-
-        difficultyPanel.setLayout(new BoxLayout(difficultyPanel, BoxLayout.PAGE_AXIS));
-        difficultyPanel.add(easyButton);
-        difficultyPanel.add(normalButton);
-        difficultyPanel.add(hardButton);
+        difficultyPanel.setLayout(new GridBagLayout());
 
         gameFrame.add(gamePanel);
         gamePanel.add(consoleTextAreaTBR);
+
+        layoutConstraints.fill = GridBagConstraints.HORIZONTAL;
+        layoutConstraints.anchor = GridBagConstraints.BASELINE;
+        layoutConstraints.insets = new Insets(10, 10, 0, 0);
+        layoutConstraints.gridx = 0;
+        layoutConstraints.gridy = 2;
+        layoutConstraints.gridwidth = 1;
+        difficultyPanel.add(easyButton, layoutConstraints);
+
+        layoutConstraints.gridx = 0;
+        layoutConstraints.gridy = 3;
+
+        difficultyPanel.add(normalButton, layoutConstraints);
+
+        layoutConstraints.gridx = 0;
+        layoutConstraints.gridy = 4;
+        difficultyPanel.add(hardButton, layoutConstraints);
+
     }
 
     public void addListener(ActionListener actionListener)
