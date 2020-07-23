b9c606c4ffd2de4b10b66c3a3398a77593b2d964
Sokratis Ousakof <skathelast@hotmail.com>
2020-07-23 14:59:22 +0100

    Basic gui done. TBD button mechanics.

--- src/MinesweeperControl.java
+++ src/MinesweeperControl.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -5,10 +6,12 @@ public class MinesweeperControl {
     MinesweeperView theView;
     MinesweeperModel theModel;
     private int numberOfMines;
+    private int easyXY = 10, normalXY = 20, hardXY = 20;
     MinesweeperControl(MinesweeperView theView,MinesweeperModel theModel){
         this.theView = theView;
         this.theModel = theModel;
-        this.theView.addListener(new minesweeperListener());
+        this.theView.setActionListener(new minesweeperListener());
+        this.theView.addListener();
     }
 
     public class minesweeperListener implements ActionListener
@@ -19,39 +22,49 @@ public class MinesweeperControl {
             if(e.getSource() == theView.easyButton)
             {
                 initialiseGameBoard(easyPreset());
-                theView.consoleTextAreaTBR.setText(theModel.displayMinefieldArray());
+                //theView.consoleTextAreaTBR.setText(theModel.displayMinefieldArray());
             }
-            if(e.getSource() == theView.normalButton)
+            else if(e.getSource() == theView.normalButton)
             {
                 initialiseGameBoard(normalPreset());
-                theView.consoleTextAreaTBR.setText(theModel.displayMinefieldArray());
+                //theView.consoleTextAreaTBR.setText(theModel.displayMinefieldArray());
             }
-            if(e.getSource() == theView.hardButton)
+            else if(e.getSource() == theView.hardButton)
             {
                 initialiseGameBoard(hardPreset());
-                theView.consoleTextAreaTBR.setText(theModel.displayMinefieldArray());
+                //theView.consoleTextAreaTBR.setText(theModel.displayMinefieldArray());
+            }
+            else if(e.getSource() instanceof JButton)
+            {
+                ((JButton)e.getSource()).setEnabled(false);
+                ((JButton)e.getSource()).setText(((JButton)e.getSource()).toString());
+                System.out.println("button pressed");
+                System.out.println(((JButton)e.getSource()).toString());
             }
         }
     }
 
     private int easyPreset(){
         numberOfMines = 25;
-        theModel.setXY(10,10);
-
+        theModel.setXY(easyXY,easyXY);
+        theView.gameFrame.setSize(500,400);
+        theView.setButtons(100, easyXY);
         return numberOfMines;
     }
 
     private int normalPreset(){
         numberOfMines = 100;
-        theModel.setXY(20,20);
-        theView.gameFrame.setSize(300,300);
+        theModel.setXY(normalXY,normalXY);
+        theView.gameFrame.setSize(800,800);
+        theView.setButtons(400, normalXY);
         return numberOfMines;
     }
 
     private int hardPreset(){
         numberOfMines = 250;
-        theModel.setXY(30,30);
-        theView.gameFrame.setSize(500,500);
+        theModel.setXY(hardXY,hardXY);
+        theView.gameFrame.setSize(800,800);
+        theView.setButtons(900, hardXY);
         return numberOfMines;
     }
 
@@ -64,6 +77,7 @@ public class MinesweeperControl {
     {
         theModel.initialiseMinefield();
         theModel.placeMines(numberOfMines);
+        theView.initialiseButtons();
         switchFrames();
     }
 }
--- src/MinesweeperView.java
+++ src/MinesweeperView.java
@@ -12,6 +12,11 @@ public class MinesweeperView {
     JButton easyButton = new JButton("Easy"), normalButton = new JButton("Normal"),
             hardButton = new JButton("Hard");
     JTextArea consoleTextAreaTBR = new JTextArea();
+    private ActionListener actionListener;
+
+    JButton[][] buttonCollection;
+
+    private int xyVariable , numOfButtons;
 
     MinesweeperView(){
         //initialise selection frame
@@ -33,13 +38,15 @@ public class MinesweeperView {
         difficultyPanel.setLayout(new GridBagLayout());
 
         gameFrame.add(gamePanel);
-        gamePanel.add(consoleTextAreaTBR);
+
+        //gamePanel.add(consoleTextAreaTBR);
 
         layoutConstraints.fill = GridBagConstraints.HORIZONTAL;
         layoutConstraints.anchor = GridBagConstraints.BASELINE;
         layoutConstraints.insets = new Insets(10, 10, 0, 0);
         layoutConstraints.gridx = 0;
         layoutConstraints.gridy = 2;
+        layoutConstraints.ipadx = 40;
         layoutConstraints.gridwidth = 1;
         difficultyPanel.add(easyButton, layoutConstraints);
 
@@ -54,10 +61,35 @@ public class MinesweeperView {
 
     }
 
-    public void addListener(ActionListener actionListener)
+    public void addListener()
     {
         easyButton.addActionListener(actionListener);
         normalButton.addActionListener(actionListener);
         hardButton.addActionListener(actionListener);
+    }
+
+    public void initialiseButtons()
+    {
+        gamePanel.setLayout(new GridLayout(xyVariable,xyVariable));
+        for(int i = 0; i <buttonCollection.length ; i ++)
+        {
+            for(int j =0 ; j < buttonCollection[0].length; j++)
+            {
+                buttonCollection[i][j] = new JButton();
+                buttonCollection[i][j].addActionListener(actionListener);
+                gamePanel.add(buttonCollection[i][j]);
+
+            }
+        }
+    }
+
+    public void setButtons(int numOfButtons, int xyVariable) {
+        this.numOfButtons = numOfButtons;
+        this.xyVariable = xyVariable;
+        buttonCollection = new JButton[xyVariable][xyVariable];
+    }
+
+    public void setActionListener(ActionListener actionListener) {
+        this.actionListener = actionListener;
     }
 }
